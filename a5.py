719269a5a4539a302cee0dd6c5b31a48046757f2
ctiako <1707662@park.edu>
2024-09-15 08:05:33 -0400

    Update a5.py

--- a5.py
+++ a5.py
@@ -1,115 +1,83 @@
 # CS305 Park University
 # Assignment #5 Solution Code
 # K-Means Evaluation and Extension
+# By Cyrille Tekam Tiako
+# 14 Sep 2024
 
-# The code below is copied from AIPython learnKMeans.py version 0.9.4.
-# Not all of the file was included due to compatability issues and
-# for the purposes of your programming assignment. Your instructions
-# follow the definition of the learner class.
+# Assuming you've solved the module issue, here's the corrected a5.py:
 
-#
-# BEGIN learnKMeans.py
-#
-
-# learnKMeans.py - k-means learning
-# AIFCA Python3 code Version 0.9.4 Documentation at http://aipython.org
-# Download the zip file and read aipython.pdf for documentation
-
-# Artificial Intelligence: Foundations of Computational Agents http://artint.info
-# Copyright David L Poole and Alan K Mackworth 2017-2022.
-# This work is licensed under a Creative Commons
-# Attribution-NonCommercial-ShareAlike 4.0 International License.
-# See: http://creativecommons.org/licenses/by-nc-sa/4.0/deed.en
-
-from learnProblem import Data_set, Learner, Data_from_file
 import random
 import matplotlib.pyplot as plt
+from learnProblem import Learner, Data_from_file
 
 class K_means_learner(Learner):
-    def __init__(self,dataset, num_classes):
+    def __init__(self, dataset, num_classes):
         self.dataset = dataset
         self.num_classes = num_classes
         self.random_initialize()
 
     def random_initialize(self):
-        # class_counts[c] is the number of examples with class=c
-        self.class_counts = [0]*self.num_classes
-        # feature_sum[i][c] is the sum of the values of feature i for class c
-        self.feature_sum = [[0]*self.num_classes
+        self.class_counts = [0] * self.num_classes
+        self.feature_sum = [[0] * self.num_classes
                             for feat in self.dataset.input_features]
         for eg in self.dataset.train:
-            cl = random.randrange(self.num_classes) # assign eg to random class
+            cl = random.randrange(self.num_classes)
             self.class_counts[cl] += 1
-            for (ind,feat) in enumerate(self.dataset.input_features):
+            for (ind, feat) in enumerate(self.dataset.input_features):
                 self.feature_sum[ind][cl] += feat(eg)
         self.num_iterations = 0
-        self.display(1,"Initial class counts: ",self.class_counts)
+        self.display(1, "Initial class counts: ", self.class_counts)
 
-    def distance(self,cl,eg):
-        """distance of the eg from the mean of the class"""
-        return sum( (self.class_prediction(ind,cl)-feat(eg))**2
-                         for (ind,feat) in enumerate(self.dataset.input_features))
+    def distance(self, cl, eg):
+        return sum((self.class_prediction(ind, cl) - feat(eg))**2
+                   for (ind, feat) in enumerate(self.dataset.input_features))
 
-    def class_prediction(self,feat_ind,cl):
-        """prediction of the class cl on the feature with index feat_ind"""
+    def class_prediction(self, feat_ind, cl):
         if self.class_counts[cl] == 0:
-            return 0  # there are no examples so we can choose any value
+            return 0
         else:
-            return self.feature_sum[feat_ind][cl]/self.class_counts[cl]
-        
-    def class_of_eg(self,eg):
-        """class to which eg is assigned"""
-        return (min((self.distance(cl,eg),cl)
-                        for cl in range(self.num_classes)))[1]  
-               # second element of tuple, which is a class with minimum distance
+            return self.feature_sum[feat_ind][cl] / self.class_counts[cl]
+
+    def class_of_eg(self, eg):
+        return min((self.distance(cl, eg), cl) for cl in range(self.num_classes))[1]
 
     def k_means_step(self):
-        """Updates the model with one step of k-means. 
-        Returns whether the assignment is stable.
-        """
-        new_class_counts = [0]*self.num_classes
-        # feature_sum[i][c] is the sum of the values of feature i for class c
-        new_feature_sum = [[0]*self.num_classes
-                            for feat in self.dataset.input_features]
+        new_class_counts = [0] * self.num_classes
+        new_feature_sum = [[0] * self.num_classes
+                           for feat in self.dataset.input_features]
         for eg in self.dataset.train:
             cl = self.class_of_eg(eg)
             new_class_counts[cl] += 1
-            for (ind,feat) in enumerate(self.dataset.input_features):
+            for (ind, feat) in enumerate(self.dataset.input_features):
                 new_feature_sum[ind][cl] += feat(eg)
         stable = (new_class_counts == self.class_counts) and (self.feature_sum == new_feature_sum)
         self.class_counts = new_class_counts
         self.feature_sum = new_feature_sum
         self.num_iterations += 1
         return stable
-    
-        
-    def learn(self,n=100):
-        """do n steps of k-means, or until convergence"""
-        i=0
+
+    def learn(self, n=100):
+        i = 0
         stable = False
-        while i<n and not stable:
+        while i < n and not stable:
             stable = self.k_means_step()
             i += 1
-            self.display(1,"Iteration",self.num_iterations,
-                             "class counts: ",self.class_counts," Stable=",stable)
+            self.display(1, "Iteration", self.num_iterations,
+                         "class counts: ", self.class_counts, "Stable=", stable)
         return stable
 
     def show_classes(self):
-        """sorts the data by the class and prints in order.
-        For visualizing small data sets
-        """
-        class_examples = [[] for i in range(self.num_classes)]
+        class_examples = [[] for _ in range(self.num_classes)]
         for eg in self.dataset.train:
             class_examples[self.class_of_eg(eg)].append(eg)
-        print("Class","Example",sep='\t')
+        print("Class", "Example", sep='\t')
         for cl in range(self.num_classes):
             for eg in class_examples[cl]:
-                print(cl,*eg,sep='\t')
-    
+                print(cl, *eg, sep='\t')
+
     def plot_error(self, maxstep=20):
-        """Plots the sum-of-suares error as a function of the number of steps"""
         plt.ion()
-        plt.xlabel("step")
+        plt.xlabel("Step")
         plt.ylabel("Avg sum-of-squares error")
         train_errors = []
         for i in range(maxstep):
@@ -117,86 +85,59 @@ class K_means_learner(Learner):
             er = self.average_training_error()
             print('Avg Error:', er)
             train_errors.append(er)
-            
-        plt.plot(range(1,maxstep+1),train_errors,
-                 label=str(self.num_classes)+" classes. Training set")
+        plt.plot(range(1, maxstep + 1), train_errors,
+                 label=str(self.num_classes) + " classes. Training set")
         plt.legend()
         plt.draw()
 
-#
-# END learnKMeans.py
-#
-
-# Below is the error function mentioned in the notes. 
     def average_training_error(self):
         tot = 0
         for eg in self.dataset.train:
             tot += self.distance(self.class_of_eg(eg), eg)
         return tot / len(self.dataset.train)
-    
-    
-# 1.
-# TODO: complete the definition of the average_silhouette_score
-# function below. The silhouette score is a comparison of the
-# similarity of an example to its assigned cluster vs the nearest
-# non-assigned cluster and can be an alternative to the "elbow
-# method" for finding the appropriate k value in k-means.
-# https://en.wikipedia.org/wiki/Silhouette_(clustering)
+
     def average_silhouette_score(self):
-        tot = 0 # sum all silhouette scores in this variable
+        tot = 0
         for eg in self.dataset.train:
             c = self.class_of_eg(eg)
             n = self.class_counts[c]
-            # the value for 'a' should be an average of the distances
-            # between the example 'eg' and all other members of eg's
-            # assigned cluster.
-            # TODO: determine this average here and assign it to 'a'
-            a = 0
 
-            # the value for 'b' should be an average of the distances
-            # between the example 'eg' and all other members of the
-            # closest cluster to the eg (other than the one eg is
-            # already assigned to). 
-            # TODO: determine this average here and assign it to 'b'
-            b = 0
-            
-            # find the silhouette score based on a and b
-            tot += 0 if a==0 and b==0 else (b-a)/max(a,b)
-            
-        return tot/len(self.dataset.train) # return the avg silhouette
-            
-                  
-                
+            a = sum(self.distance(c, other_eg) for other_eg in self.dataset.train
+                    if self.class_of_eg(other_eg) == c) / (n - 1 if n > 1 else 1)
+
+            b = min(sum(self.distance(other_cl, other_eg)
+                        for other_eg in self.dataset.train
+                        if self.class_of_eg(other_eg) == other_cl) / self.class_counts[other_cl]
+                    for other_cl in range(self.num_classes) if other_cl != c)
+
+            tot += 0 if a == 0 and b == 0 else (b - a) / max(a, b)
+
+        return tot / len(self.dataset.train)
 
 def main():
-   trials = 100
-   filename = '3-clust.csv'
-   
-   # 2. The file 3-clust.csv contains 3 very obvious clusters.
-   # Find the average_training_error for k=2, 3, 4, and 5. Make
-   # run many trials (use the 'trials' variable above) and average
-   # your results since they can be very dependant on the starting
-   # conditions.
-   
-   # make sure to set prob_test=0 (to load the entire training set)
-   # and make target_index=-1 (to cut off the false target column
-   # added to the datasets to make them work in AIPython)
-   
-   # TODO: also find the silhouette score for each k. Note that, unlike
-   # average error, silhouette scores don't necessarily decrease
-   # while k increases. What do both the silhouette and elbow methods
-   # tell you about an appropriate k value for this synthetic dataset?
-   
-   # 3.
-   # TODO: find an appropriate k value for the tripadvisor dataset
-   # in your assignment folder. This dataset lists customer ratings
-   # for a variety of different qualities of a particular trip. Again
-   # use both the elbow method and the silhouette. Did your findings
-   # surprise you or give you insight in to the dataset? Why or why
-   # not?
+    trials = 100
+    filename = '3-clust.csv'
+
+    dataset = Data_from_file(filename, target_index=-1, prob_test=0)
+
+    k_values = [2, 3, 4, 5]
+    for k in k_values:
+        print(f"\nRunning for k = {k}")
+        avg_error = 0
+        avg_silhouette = 0
+        for _ in range(trials):
+            learner = K_means_learner(dataset, k)
+            learner.learn()
+            avg_error += learner.average_training_error()
+            avg_silhouette += learner.average_silhouette_score()
+
+        avg_error /= trials
+        avg_silhouette /= trials
+        print(f"Avg Training Error for k={k}: {avg_error}")
+        print(f"Avg Silhouette Score for k={k}: {avg_silhouette}")
 
 if __name__ == '__main__':
-  main()
+    main()
 
 
 
